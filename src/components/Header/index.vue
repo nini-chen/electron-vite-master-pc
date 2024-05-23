b7d963d62948f8da87d44ee3ea159d0cab7126c5
neon_chen <1826650603@qq.com>
2024-05-23 10:23:15 +0800

    init:初始化

---
+++ src/components/Header/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+// import { ref } from "vue"
+</script>
+<template>
+  <div>
+    <div class="icon">
+      <img src="@/assets/images/login_ITC.png" />
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+// .icon {
+// width: 100%;
+// height: 100%;
+// display: flex;
+// justify-content: center;
+// align-items: center;
+// img {
+//   width: 100%;
+//   height: 100%;
+// }
+// }
+</style>
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+// import { useUserStore } from "@/store/modules/user"
+// import Admin from "./components/Admin.vue"
+// import Editor from "./components/Editor.vue"
+import Heard from "../../components/Header/index.vue"
+
+// const userStore = useUserStore()
+// const isAdmin = userStore.roles.includes("admin")
+</script>
+
+<template>
+  <!-- <component :is="isAdmin ? Admin : Editor" /> -->
+  <Heard />
+</template>
